c5a8b012edc68b83fdd37523b8cecb37436f0049
Michael Quitaleg Soriano <michael.soriano@starfi.sh>
2013-05-14 10:22:46 +0800

    careers landing page

--- Project/Code/1_Website/Pages/primary/careers/Controller.php
+++ Project/Code/1_Website/Pages/primary/careers/Controller.php
@@ -2,76 +2,19 @@
 require_once FILE_ACCESS_CORE_CODE.'/Framework/MVC_superClasses_Core/controllerSuperClass_Core/controllerSuperClass_Core.php';
 require_once('Model.php');
 require_once('View.php');
-require_once('Project/Code/System/Routes/route.php');
 
 class controller extends controllerSuperClass_Core 
 {
 	public function indexAction()
 	{
-		$dataHandler = new dataHandler();
+		$model = new model();
 		$view = new view();
-		$view->_XMLObj = $dataHandler->loadDataSimpleXML('Data/1_Website/Content/Pages/primary/careers/data.xml');
 		
-		$url_parameters = routes::getInstance()->_pathInfoArray;
+		$XML = $model->loadDataSimpleXML('data');
+		$view->_XMLObj = $XML;
 		
-		if(count($url_parameters) == 1){
-			$this->careersListing();
-		}
-		if (count($url_parameters) == 2) {
-			if(route::ifPermalinkExists($url_parameters[1])) {
-				$this->careersDetails($url_parameters[1]);
-			} else {
-				header('location: /careers');
-			}
-		}
-		if (count($url_parameters) > 2) {
-			header('location: /careers');
-		}
-	}
-	public function careersListing() {
-		require_once 'Project/Code/System/Job_Manager/jobs.php';
-	
-		$jobs = new jobs();
-		$jobs->select();
-		$array_of_jobs = $jobs->getArrayOfJobs();
-		
-		$view = new view();
-		$view->_set('array_of_jobs', $array_of_jobs);
-		$view->displayCareersListing();
-	}	
-	public function careersDetails($permalink = NULL) {
-		require_once 'Project/Code/System/Job_Manager/job.php';
-		
-		$job = new job();
-		$job->setPermalink($permalink);
-		$job->select();
-		
-		$view = new view();
-		$view->_set('jobs', $job);
-		$view->displayAppForm();
-	}	
-	public function	submitApplicationAjaxAction() {
-		$name = $_REQUEST['name'];
-		$desiredPosition = $_REQUEST['desiredPosition'];
-		$mobile = $_REQUEST['mobile'];
-		$email = $_REQUEST['email'];
-		$message = $_REQUEST['message'];
-		
-		$body = "
-					Name: $name
-					
-					Desired Position: $desiredPosition
-		
-					Contact Number: $mobile
-					
-					Email Address: $email
-					
-					Message: $message
-				";
-		
-		//mail('jephjeph02@gmail.com', 'New inquiry received', $body, 'New inquiry received');
-		mail('joseph.reyes@starfi.sh', 'New inquiry received', $body, 'New inquiry received');
-		jQuery::getResponse();		
+		$view->_set('array_of_jobs', $model->getPositions());
+		$view->renderAll();
 	}
 }
 ?>
--- Project/Code/1_Website/Pages/primary/careers/Model.php
+++ Project/Code/1_Website/Pages/primary/careers/Model.php
@@ -1,8 +1,20 @@
 <?php
 require_once FILE_ACCESS_CORE_CODE.'/Framework/MVC_superClasses_Core/modelSuperClass_Core/modelSuperClass_Core.php';
+
 class model extends modelSuperClass_Core
 {
-
-
+	private $array_of_jobs;
+	private $file_dir = 'Data/1_Website/Content/Pages/primary/careers';
+	private $files = array('position1','position2','position3','position4','position5');	
+	
+	public function getPositions()
+	{
+		foreach ($this->files as $file)
+		{
+			$xml_data = simplexml_load_file($this->file_dir.'/'.$file.'.xml');
+			$this->array_of_jobs[] = $xml_data->job_title_element;
+		}
+		return $this->array_of_jobs;
+	}	
 }
 ?>
